2c4ef147c27a5a271af31735ffd29b4253ac3144
basfar <mukhtaronif@gmail.com>
2023-12-10 23:20:53 +0000

    Updated: still bored

--- src/main/java/com/mukky/bored/config/WebSocketEventListener.java
+++ src/main/java/com/mukky/bored/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.mukky.bored.config;
 
+import com.mukky.bored.dto.ChatMessage;
+import com.mukky.bored.dto.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,21 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations message;
+
     @EventListener
     public void handleWebDisconnectListener(SessionDisconnectEvent event){
-        // TODO --- to be done later
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) accessor.getSessionAttributes().get("username");
+
+        if(username != null){
+            log.info("User Disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .messageType(MessageType.LEAVER)
+                    .sender(username)
+                    .build();
+            message.convertAndSend("/topic/public",chatMessage);
+
+        }
     }
 }
--- src/main/java/com/mukky/bored/controller/ChatController.java
+++ src/main/java/com/mukky/bored/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.mukky.bored.dto.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,5 +14,13 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
          return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor accessor){
+        accessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
     }
 }
